b38f0a8e7b9b389b1f64eb436e47c7b13b5b0a06
samsvk <samsvk00@gmail.com>
2022-11-25 16:42:12 +0000

    requires

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const {
+  Client,
+  GatewayIntentBits,
+  Collection,
+  REST,
+  Routes,
+} = require("discord.js");
+const fs = require("fs");
+const path = require("path");
