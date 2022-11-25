24b3a67a1b0a231144c9ad146acf424e10a64a2b
samsvk <samsvk00@gmail.com>
2022-11-25 18:36:11 +0000

    function handlers working

--- src/functions/events/ready.js
+++ src/functions/events/ready.js
@@ -1,7 +1,22 @@
+const { REST, Routes } = require("discord.js");
+
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
-    console.log(`${client.user.tag} iso online`);
+    console.log(`${client.user.tag} is online`);
+
+    const guild_ids = client.guilds.cache.map((guild) => guild.id);
+    const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
+    for (const guildId of guild_ids) {
+      rest
+        .put(Routes.applicationGuildCommands(process.env.CLI_ID, guildId), {
+          body: client.commandArray,
+        })
+        .then(() =>
+          console.log("Successfully updated commands for guild " + guildId)
+        )
+        .catch(console.error);
+    }
   },
 };
