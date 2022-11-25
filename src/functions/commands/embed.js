26a24693b19a79254dc5ebceed00512f01eb47a4
samsvk <samsvk00@gmail.com>
2022-11-25 18:52:41 +0000

    fixed

--- src/functions/commands/embed.js
+++ src/functions/commands/embed.js
@@ -1,11 +1,15 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("status").setDescription("mommy milkers"),
+  data: new SlashCommandBuilder()
+    .setName("status")
+    .setDescription("Replies with Vi's commission status"),
   async execute(interaction, client) {
     const embed = new EmbedBuilder()
       .setTitle("Vi's commissons are currently open")
-      .setDescription("Bipbop")
+      .setDescription(
+        "Commissions are currently open with 3/5 slots filled. Please visit vivixstar.com/information on how to commission me!"
+      )
       .setColor(15548997)
       .setThumbnail(`${client.user.displayAvatarURL()}`);
     await interaction.reply({ embeds: [embed] });
