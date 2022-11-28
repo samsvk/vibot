874a741980d663b39ceab700153c2ba7b91ec18e
samsvk <samsvk00@gmail.com>
2022-11-28 18:22:50 +0000

    fixed parsing

--- src/functions/modals/comissionModal.js
+++ src/functions/modals/comissionModal.js
@@ -1,3 +1,4 @@
+const { bold } = require("discord.js");
 module.exports = {
   id: "commissionModal",
   async execute(interaction, client) {
@@ -16,36 +17,25 @@ module.exports = {
       value: item.value,
     }));
 
-    const style = cachedFields.find(
-      (item) => item.name === "modelStyle" || item.name === "characterStyle"
-    );
-    const details = cachedFields.find(
-      (item) => item.name === "modelDetails" || item.name === "characterDetails"
-    );
-    const line = cachedFields.find(
-      (item) => item.name === "modelLine" || item.name === "characterLine"
-    );
-    const commercial = cachedFields.find(
-      (item) =>
-        item.name === "modelCommercial" || item.name === "characterCommercial"
-    );
-    const background = cachedFields.find(
-      (item) => item.name === "characterBackground"
-    );
+    const values = cachedFields.map((item) => ({
+      name: Object.values(item)[0].split(/(?=[A-Z])/)[1],
+      value: Object.values(item)[1],
+    }));
+
+    const sentence = `${bold("New:")} ${
+      modalType ? "Live 2D Model" : "Character Art"
+    } commission from: ${interaction.user.username}#${
+      interaction.user.discriminator
+    } — ${values.map((item) => `${item.name}: ${item.value}`).join(", ")}`;
 
     if (modalType) {
-      await channel.send({
-        content: `NEW Live 2d Model Art commission from ${interaction.user.username}#${interaction.user.discriminator} > commission style: ${style.value}, details: ${details.value}, line style: ${line.value} and commercial rights: ${commercial.value}`,
-      });
-
+      channel.send({ content: sentence });
       await interaction.reply({
         content: `Commission created.`,
         ephemeral: true,
       });
     } else {
-      await channel.send({
-        content: `NEW Character Art commission from ${interaction.user.username}#${interaction.user.discriminator} > commission style: ${style.value}, details: ${details.value}, line style: ${line.value} background: ${background.value} and commercial rights: ${commercial.value}`,
-      });
+      channel.send({ content: sentence });
       await interaction.reply({
         content: `Commission created.`,
         ephemeral: true,
