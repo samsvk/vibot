b3c08a53bccd3d3eafcefed49c4f4c32b0447bd5
samsvk <samsvk00@gmail.com>
2022-12-06 12:56:47 +0000

    refactor

--- src/functions/handleInteractions.js
+++ src/functions/handleInteractions.js
@@ -1,12 +1,14 @@
 const interactionTypes = ["buttons", "modals", "commands", "selectmenus"];
 
+function getFiles(name, fs, path) {
+  return fs
+    .readdirSync(path.join(__dirname, `/${name}`))
+    .filter((file) => file.endsWith(".js"));
+}
+
 module.exports = (client, fs, path, Collection) => {
   client.handleEvents = async () => {
-    const eventFiles = fs
-      .readdirSync(path.join(__dirname, "/events"))
-      .filter((file) => file.endsWith(".js"));
-
-    for (const file of eventFiles) {
+    for (const file of getFiles("events", fs, path)) {
       const event = require(path.join(__dirname, "/events", file));
       if (event.once) {
         client.once(event.name, (...args) => event.execute(...args, client));
@@ -19,12 +21,7 @@ module.exports = (client, fs, path, Collection) => {
   client.handleInteractions = async () => {
     interactionTypes.map((type) => {
       client[type] = new Collection();
-
-      const typeFiles = fs
-        .readdirSync(path.join(__dirname, `/${type}`))
-        .filter((file) => file.endsWith(".js"));
-
-      for (const file of typeFiles) {
+      for (const file of getFiles(type, fs, path)) {
         const _ = require(path.join(__dirname, `/${type}`, file));
         client[type].set(_.id || _.data.name, _);
       }
